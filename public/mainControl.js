982705ba7f94ed7c69c7a80932d079b5df04ffc7
Lauxx <lm.nichols@hotmail.com>
2016-07-12 15:04:01 -0600

    working my way through angular

--- public/mainControl.js
+++ public/mainControl.js
@@ -1,3 +1,17 @@
 angular.module('chatApp').controller('chatController', function($scope, chatServe){
 	$scope.hello = chatServe.welcome;
+
+	$scope.chat;
+	// $scope.postChat = function(){
+	// 	chatServe.postChat(chat).then(function(res){
+	// 		$scope.chat = res.data;
+	// 		console.log(res.data);
+	// 	})
+	// }
+	$scope.getChat = function(){
+		chatServe.getChat().then(function(res){
+			$scope.chat = res;
+			console.log($scope.chat, 'i am control data');
+		})
+	}
 });
--- public/mainServe.js
+++ public/mainServe.js
@@ -1,5 +1,24 @@
 angular.module('chatApp').service('chatServe', function($http){
-	
+
 	this.welcome="Hello Lauren";
 
+	// this.postChat = function(){
+	// 	return $http({
+	// 		method: 'POST',
+	// 		url: '/chat'
+	// 	}).then(function(res){
+	// 		console.log(res.data);
+	// 		return res.data;
+	// 	})
+	// };
+	this.getChat = function(){
+		return $http({
+			method: 'GET', 
+			url: '/chat'
+		}).then(function(res){
+			console.log(res.data, 'i am service data');
+			return res.data
+		})
+	};
+	
 });
